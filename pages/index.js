ad23ef30ef37566dd9745c36530853d15bfdae42
AfsiyaNaik <afsiyanaik734@gmail.com>
2025-08-23 19:09:47 +0530

    Initial commit - WDC website

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { Layout } from "antd";
 import HeaderBanner from "../components/HeaderBanner";
+import AboutUs from "../components/AboutUs";
 import QuickLinks from "../components/QuickLinks";
-import DevelopmentSectors from '../components/DevelopmentSectors';
+import DevelopmentSectors from "../components/DevelopmentSectors";
 // import ContactWDC from "@/components/ContactWDC";
 
 const { Content, Footer } = Layout;
 
-const Home = () => {
+export default function Home() {
   return (
     <Layout>
+      {/* Header Section */}
       <HeaderBanner />
-      
 
-      { <Content style={{ padding: "20px" }}>
+      {/* Main Content */}
+      <Content style={{ padding: "20px" }}>
+        <AboutUs />
         <QuickLinks />
-         <DevelopmentSectors /> 
+        <DevelopmentSectors />
         {/* <ContactWDC /> */}
-      </Content> }
+      </Content>
 
+      {/* Footer */}
       <Footer style={{ textAlign: "center" }}>
         Ward Development Committee ©2025 | Powered by React + Next.js + Ant Design
       </Footer>
     </Layout>
   );
-};
-
-export default Home;
+}
